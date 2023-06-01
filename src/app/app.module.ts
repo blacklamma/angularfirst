05b36181843b3af7b5ff4ecacd4aba53e6930e5b
blacklamma <rudradeep1998@gmail.com>
2023-06-01 19:14:31 +0530

    trying bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
